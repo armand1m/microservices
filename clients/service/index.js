bdcf6c8f26502d5664d725c63f1d767d3a113a10
Armando Magalhães <armando.mag95@gmail.com>
2016-06-08 13:36:04 -0300

    feat(project): Client service in 70%
    Client service now has a persistence unit (RethinkDB container);
    Client service now has its own model of data;
    Client service now has a router for the request methods, so it can fully implement a CRUD operation as a service;
    Client service now does not send any data back to the client, this need to be fixed;

--- clients/service/index.js
+++ clients/service/index.js
@@ -1,9 +1,65 @@
-import { json, send } from 'micro';
+import micro, { send, sendError, json } from 'micro';
+import thinky from 'thinky';
 
-export default async function (req, res) {
-  const data = await json(req);
+const type = thinky.type;
 
-  console.log(data.name);
-  
-  send(res, 200, data);
+const db = thinky({
+  host: "database",
+  port: process.env.DB_PORT,
+  authKey: "",
+  db: "clients"
+});
+
+const Client = db.createModel("Client", {
+  id: type.string(),
+  name: type.string().required(),
+  email: type.string().email().required(),
+  birthday: type.date(),
+  active: type.boolean().default(true)
+});
+
+const save = async (request, response) => {
+  var data = await json(request);
+
+  new Client(data)
+  .save()
+  .then(client => {
+    console.log(client);
+    return client;
+  });
+}
+
+const all = async (request, response) => {
+  Client
+  .run()
+  .then(clients => {
+    console.log(clients);
+    return clients;
+  });
+}
+
+const router = async (request, response) => {
+  try {
+    switch (request.method) {
+      case 'POST':
+        return save(request, response);
+
+      case 'GET':
+        return all(request, response);
+
+      default:
+        send(response, 405, 'Invalid method');
+        break;
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+export default async function (request, response) {
+  try {
+    send(response, 200, await router(request, response));
+  } catch (error) {
+    sendError(request, response, error);
+  }
 }
