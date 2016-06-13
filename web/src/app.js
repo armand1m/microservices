314e6ee06c758afde332565a7317650f8f7c69b1
Armando Magalhães <armando.mag95@gmail.com>
2016-06-13 08:27:14 -0300

    chore(web): Started developing web application

--- web/src/app.js
+++ web/src/app.js
@@ -1,14 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import ReactComponent from './components/react-component';
 
-const App = () => (
-  <MuiThemeProvider muiTheme={getMuiTheme()}>
-    <ReactComponent />
-  </MuiThemeProvider>
-);
+import { cyan500 } from 'material-ui/styles/colors';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: cyan500,
+  },
+  appBar: {
+    height: 50,
+  },
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  test(e) {
+    console.log("clicked", e);
+  }
+
+  handleToggle() {
+    this.setState({open: !this.state.open});
+  }
+
+  render() {
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div>
+          <Drawer open={this.state.open}>
+            <MenuItem>Menu Item</MenuItem>
+            <MenuItem>Menu Item 2</MenuItem>
+          </Drawer>
+          <AppBar title="My AppBar" onTitleTouchTap={this.handleToggle}/>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
 
 ReactDOM.render(
   <App />,
